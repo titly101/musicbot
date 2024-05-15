02ad01a1e17ea2074b577cc1740adf74b8a20dfb
aayu820 <168717255+aayu820@users.noreply.github.com>
2024-05-15 08:27:57 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -43,7 +43,7 @@ async def init():
         await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AviaxMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot...."
         )
         exit()
     except:
@@ -55,7 +55,7 @@ async def init():
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AviaxMusic").info("Stopping Aviax Music Bot...")
+    LOGGER("AviaxMusic").info("Stopping Aviax Music Bot....")
 
 
 if __name__ == "__main__":
